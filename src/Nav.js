e80c29e0a4caafdf08a107ec21535e94e96f1b43
anza04 <64538739+anza04@users.noreply.github.com>
2022-01-31 11:11:22 +0100

    init commit

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <div className="links">
+        <a href="#">discover</a>
+        <a href="#">home</a>
+        <a href="#">contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
